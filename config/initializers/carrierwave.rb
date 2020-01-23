ac1af5abce2be38f63d090d0a3d113f995399d81
kazuya hojo <maboai.kazu29@gmail.com>
2020-01-23 14:44:27 +0900

    change/image_uploader.rb編集
    MiniMagickの適用
    投稿時の画像サイズの指定
    carrierwave.rbの構文エラー解消

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,4 +18,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'freemarket-sample-61a'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-61a'
+  end
 end
