f6ed51bf2eb792afb921f58c828695cc2afe6fac
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-02-13 22:21:47 +0900

    edit/コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :tops, only: [:index]
   resources :items_sell, only: [:index]
   resources :item_informations, only: [:index]
+  resources :users, only: [:index] do
+    collection do
+      get 'tel'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
