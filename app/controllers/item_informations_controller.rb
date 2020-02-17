49ecd5f7b321568a644754e2893ec831f73816a1
kuroron985 <shigotoyou.taka@gmil.com>
2020-02-17 16:07:41 +0900

    Merge branch 'master' into add/items-sell_page2

---
+++ app/controllers/item_informations_controller.rb
@@ -0,0 +1,6 @@
+class ItemInformationsController < ApplicationController
+
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   root "tops#index"
 
   resources :tops, only: [:index]
-
-
-
-
-
-
-
-
   resources :item_sells, only: [:index]
+  resources :item_informations, only: [:index]
+  resources :users, only: [:index, :new] do
+    collection do
+      get 'tel'
+    end
+  end
 end
