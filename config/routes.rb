4ebb8f051a12f986ae559b7fe6ddeddf1354c504
kuroron985 <shigotoyou.taka@gmil.com>
2020-02-17 16:03:43 +0900

    [fix] ルーティングの記述位置変更
    musterブランチ12行目#For~
    のコメントアウトの部分を削除
    コンフリクト解消のため、記述位置は13行目に

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root "tops#index"
 
   resources :tops, only: [:index]
+
+
+
+
+
+
+
+
   resources :item_sells, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
