30eb25fe0a9d5d0761ca9bccb65659d7a6409b19
kazuya hojo <maboai.kazu29@gmail.com>
2020-02-17 09:54:25 +0900

    add/ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :tops, only: [:index]
   resources :items_sell, only: [:index]
   resources :item_informations, only: [:index]
+  resources :item_buys, only: [:index] do
+    collection do
+      get 'done'
+    end
+  end
   resources :users, only: [:index, :new] do
     collection do
       get 'tel'
