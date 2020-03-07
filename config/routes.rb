6cfd1b9c8c80198b8e37f10da63a9737a790e4d8
kazuya hojo <maboai.kazu29@gmail.com>
2020-03-07 16:34:37 +0900

    shange/ルーティング変更
    createアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       get :login
     end
   end
-  resources :items, only: [:index, :new, :show] do
+  resources :items, only: [:index, :new, :create, :show] do
     collection do
       get 'confirmation'
       get 'complete'
