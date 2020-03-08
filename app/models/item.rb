b17e38f995bae509514c634b9cc5383c7ee9633c
kazuya hojo <maboai.kazu29@gmail.com>
2020-03-08 15:34:54 +0900

    add/user-itemアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_many :images
+  belongs_to :user
   # accepts_nested_attributes_for→　fields_forメソッドを利用する際に、親モデルの中に書く必要があるメソッド。引数として子モデルの名前を記述。
   # allow_destroy→　親のレコードが削除された場合に、関連付いている子のレコードも一緒に削除される。
   accepts_nested_attributes_for :images, allow_destroy: true
