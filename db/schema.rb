a6a40b24710fe7ebd5f9e06bfb193408dceb67ce
kazuya hojo <maboai.kazu29@gmail.com>
2020-03-08 15:39:48 +0900

    add/不要なカラムを削除
    user_idカラム追加時、誤って作成された「user_id_id」カラムを消去。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_071726) do
+ActiveRecord::Schema.define(version: 2020_03_08_062836) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2020_03_07_071726) do
     t.string "category", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema.define(version: 2020_03_07_071726) do
   end
 
   add_foreign_key "images", "items"
+  add_foreign_key "items", "users"
 end
