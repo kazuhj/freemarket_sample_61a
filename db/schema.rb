130b4e662686731dcd28019e9e808a24dccbcc42
kazuya hojo <maboai.kazu29@gmail.com>
2020-03-09 22:48:56 +0900

    add/sales_statusカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_062836) do
+ActiveRecord::Schema.define(version: 2020_03_09_080127) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_03_08_062836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "sales_status"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
