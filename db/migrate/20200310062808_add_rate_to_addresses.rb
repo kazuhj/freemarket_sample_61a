67b823c294ce1c30ed8804a6bb907d08dd84881c
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-10 16:03:24 +0900

    add_to/Addressesテーブルに必要なカラムを追加

--- db/migrate/20200310062808_add_rate_to_addresses.rb
+++ db/migrate/20200310062808_add_rate_to_addresses.rb
@@ -1,4 +1,12 @@
 class AddRateToAddresses < ActiveRecord::Migration[5.2]
   def change
+    add_reference :addresses, :user, foreign_key: true
+    add_column :addresses, :family_name,      :string, null: false
+    add_column :addresses, :first_name,       :string, null: false
+    add_column :addresses, :family_name_kana, :string, null: false
+    add_column :addresses, :first_name_kana,  :string, null: false
+    add_column :addresses, :zip_code,         :string, null:false
+    add_column :addresses, :adress,           :string, null: false
+    add_column :addresses, :building,         :string
   end
 end
