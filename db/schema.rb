fba61e0bbdc241dd9d3261ea4e1a282bb0a06e4c
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-10 17:02:42 +0900

    add_to/cityカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_072517) do
+ActiveRecord::Schema.define(version: 2020_03_10_073607) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_072517) do
     t.string "zip_code", null: false
     t.string "adress", null: false
     t.string "building"
+    t.string "city", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
