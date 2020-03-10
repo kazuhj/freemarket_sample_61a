a1885fb97a14b95852626e385066fc0d037bac73
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-10 19:44:07 +0900

    add_to/Addressモデルにバリデーションを設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,5 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :zip_code, :prefecture, :city, :address ,presence: true
 end
