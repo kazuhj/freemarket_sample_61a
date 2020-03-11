10d10887c12630c45c1cbd362c657aa47870cafc
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-11 18:03:22 +0900

    add/cardsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_073607) do
+ActiveRecord::Schema.define(version: 2020_03_11_085028) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2020_03_10_073607) do
     t.string "building"
     t.string "city", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "customer_number", null: false
+    t.integer "year", null: false
+    t.integer "month", null: false
+    t.integer "security_code", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
