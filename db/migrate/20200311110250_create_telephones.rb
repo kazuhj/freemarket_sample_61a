604f7d391ed01c5cb46079622cd1774d9d57aadd
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-11 20:13:26 +0900

    add/telephonesテーブルのカラムを作成

--- db/migrate/20200311110250_create_telephones.rb
+++ db/migrate/20200311110250_create_telephones.rb
@@ -1,7 +1,8 @@
 class CreateTelephones < ActiveRecord::Migration[5.2]
   def change
     create_table :telephones do |t|
-
+      t.string     :tel,  null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_094411) do
+ActiveRecord::Schema.define(version: 2020_03_11_110250) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2020_03_11_094411) do
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
+  create_table "telephones", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "tel", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_telephones_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "family_name", null: false
@@ -63,4 +71,5 @@ ActiveRecord::Schema.define(version: 2020_03_11_094411) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
+  add_foreign_key "telephones", "users"
 end
