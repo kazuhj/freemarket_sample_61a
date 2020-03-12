1658da198eaeb74216566317becc1894aa71d7c8
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-12 18:48:13 +0900

    edit/addressesテーブルのカラム名の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_110250) do
+ActiveRecord::Schema.define(version: 2020_03_12_094316) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_110250) do
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
     t.string "zip_code", null: false
-    t.string "adress", null: false
+    t.string "address", null: false
     t.string "building"
     t.string "city", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
