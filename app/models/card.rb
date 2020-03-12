ad16581e138070635ffb320fe921507471fc602f
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-12 19:09:33 +0900

    add_to/cardモデルとuserモデルのアソシエーション設定とcardモデルのバリデーション設定

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,4 @@
 class Card < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :customer_number, :year, :month, :security_code ,presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day ,presence: true
   has_one :telephone
   has_one :address
+  has_one :card
 end
