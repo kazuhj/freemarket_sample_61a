4bc76d6203c01356f9d6fa388f698056ccb56ff3
kazuya hojo <maboai.kazu29@gmail.com>
2020-03-13 14:18:55 +0900

    change/バリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,8 @@ class Item < ApplicationRecord
   # allow_destroy→　親のレコードが削除された場合に、関連付いている子のレコードも一緒に削除される。
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  validates :name, presence: true
-  validates :text, presence: true
+  validates :name, length: { in: 1..40 }, presence: true
+  validates :text, length: { in: 1..1000 }, presence: true
   validates :condition, presence: true
   validates :price, presence: true
   validates :fee_burden, presence: true
