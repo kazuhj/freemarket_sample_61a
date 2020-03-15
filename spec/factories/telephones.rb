8893bd5880098cd762a2a14f9d3565eac4517388
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-15 21:51:39 +0900

    add/単体テスト_telephoneモデルのバリデーションテスト

--- spec/factories/telephones.rb
+++ spec/factories/telephones.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :telephone do
-    
+    id      {1}
+    tel     {"00000000000"}
+    user_id {"1"}
   end
 end
--- spec/models/telephone_spec.rb
+++ spec/models/telephone_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe Telephone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Telephone do
+  describe '#create' do
+    # 1. tel、user_idが存在すれば登録できること
+    it "tel、user_idが存在すれば登録できます" do
+      telephone = build(:telephone)
+      expect(telephone).to be_valid
+    end
+
+    # 2. telが空では登録できないこと
+    it "telが空だと登録出来ません" do
+      telephone = build(:telephone, tel: nil)
+      telephone.valid?
+      expect(telephone.errors[:tel]).to include("can't be blank")
+    end
+
+    # 3. telが半角数字の11桁だと登録できること
+    it "telが半角数字の11桁だと登録出来ます" do
+      telephone = build(:telephone, tel: "00000000000")
+      telephone.valid?
+      expect(telephone).to be_valid
+    end
+
+    # 4. telが9桁以下では登録できないこと
+    it "telが9桁以下だと登録出来ません " do
+      telephone = build(:telephone, tel: "000000000")
+      telephone.valid?
+      expect(telephone.errors[:tel]).to include("は11桁で入力して下さい")
+    end
+
+    # 5. telが12桁以上では登録できないこと
+    it "telが12桁以上だと登録出来ません " do
+      telephone = build(:telephone, tel: "000000000000")
+      telephone.valid?
+      expect(telephone.errors[:tel]).to include("は11桁で入力して下さい")
+    end
+
+    # 6. user_idが空では登録できないこと
+    it "user_idが空だと登録出来ません" do
+      telephone = build(:telephone, user_id: nil)
+      telephone.valid?
+      expect(telephone.errors[:user_id]).to include("can't be blank")
+    end
+  end
 end
