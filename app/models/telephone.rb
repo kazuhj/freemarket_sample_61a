d7d132f5bd78eaaa82fb030ef4d1789e7da7fb3f
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-15 16:03:30 +0900

    add_to/telephoneモデルのバリデーション追加

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -1,4 +1,8 @@
 class Telephone < ApplicationRecord
+
+  # アソシエーション
   belongs_to :user, optional: true
-  validates :tel ,presence: true
+
+  # バリデーション
+  validates :tel ,presence: true, format: { with: /\A\d{10,11}\z/} # ハイフンなしの10桁 or 11桁
 end
