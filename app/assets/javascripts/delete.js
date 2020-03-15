30b7c109a627a8ec78ec0f7e62bc61ac97458501
kazuya hojo <maboai.kazu29@gmail.com>
2020-03-15 16:13:27 +0900

    change/delete.js記述変更
    turbolinksを使用した書き方に変更

--- app/assets/javascripts/delete.js
+++ app/assets/javascripts/delete.js
@@ -1,5 +1,7 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
+
   $('.btn-delete').click(function(){
+    console.log('hoge')
     var alert = "確認\n削除すると２度と復活できません。\n削除する代わりに停止することもできます。\n本当に削除しますか？"
     if(!confirm(alert)){
       return false;
