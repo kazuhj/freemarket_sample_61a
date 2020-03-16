50f56f9f4665fd11952c914c42f82c7e8dcde3bf
kanayan823 <kanayan.yan823@i.softbank.jp>
2020-03-16 16:45:43 +0900

    fix/単体テスト修正userモデルのバリデーションテスト
    以前の記述だと単体テストの際にエラーが発生していたため修正。エラー解消し問題なく通りました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,14 +12,14 @@ describe User do
     it "ニックネームが空だと登録出来ません" do
       user = build(:user, nickname: nil)
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(user.errors[:nickname]).to include("を入力してください")
     end
 
     # 3. emailが空では登録できないこと
     it "emailが空だと登録出来ません" do
       user = build(:user, email: nil)
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include("を入力してください")
     end
 
     # 4. 重複したemailが存在する場合登録できないこと
@@ -27,56 +27,56 @@ describe User do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
     # 5. passwordが空では登録できないこと
     it "passwordが空だと登録出来ません" do
       user = build(:user, password: nil)
       user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include("を入力してください","は不正な値です")
     end
 
     # 6. passwordが半角英数字の7文字以上であれば登録できること
     it "passwordが半角英数字の7文字以上であれば登録出来ます" do
-      user = build(:user, password: "a000000")
+      user = build(:user, password: "a000000", password_confirmation: "a000000")
       user.valid?
       expect(user).to be_valid
     end
 
     # 7. passwordが６文字以下では登録できないこと
     it "passwordが6文字以下だと登録出来ません " do
-      user = build(:user, password: "000000")
+      user = build(:user, password: "000000", password_confirmation: "000000")
       user.valid?
-      expect(user.errors[:password]).to include("は7文字以上で入力して下さい")
+      expect(user.errors[:password]).to include("は7文字以上で入力してください")
     end
 
     # 8. passwordが7文字以上でも半角英数字以外では登録できないこと
     it "passwordが7文字以上でも半角英数字以外だと登録出来ません " do
-      user = build(:user, password: "0_00000")
+      user = build(:user, password: "0_00000", password_confirmation: "0_00000")
       user.valid?
-      expect(user.errors[:password]).to include("は半角英数字で入力して下さい")
+      expect(user.errors[:password]).to include("は不正な値です")
     end
 
     # 9. family_nameが空だと登録出来ないこと
     it "family_nameが空だと登録出来ません" do
       user = build(:user, family_name: nil)
       user.valid?
-      expect(user.errors[:family_name]).to include("can't be blank")
+      expect(user.errors[:family_name]).to include("を入力してください")
     end
 
     # 10. first_nameが空だと登録出来ないこと
     it "first_nameが空だと登録出来ません" do
       user = build(:user, first_name: nil)
       user.valid?
-      expect(user.errors[:first_name]).to include("can't be blank")
+      expect(user.errors[:first_name]).to include("を入力してください")
     end
 
     # 11. family_name_kanaが空だと登録出来ないこと
     it "family_name_kanaが空だと登録出来ません" do
       user = build(:user, family_name_kana: nil)
       user.valid?
-      expect(user.errors[:family_name_kana]).to include("can't be blank")
+      expect(user.errors[:family_name_kana]).to include("を入力してください", "は不正な値です")
     end
 
     #12. family_name_kanaはカタカナorひらがなで登録OK
@@ -85,17 +85,18 @@ describe User do
       expect(user).to be_valid
     end
 
-    #13. family_name_kanaはカタカナorひらがな以外では登録出来ないこと
-    it "family_name_kanaはカタカナorひらがな以外では登録出来ません" do
+    # 13. family_name_kanaがカタカナorひらがな以外では登録できないこと
+    it "family_name_kanaはカタカナorひらがな以外では登録出来ません " do
       user = build(:user, family_name_kana: "katakana")
-      expect(user.errors[:family_name_kana]).to include("はカタカナorひらがなで入力して下さい")
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("は不正な値です")
     end
 
     # 14. first_name_kanaが空だと登録出来ないこと
     it "first_name_kanaが空だと登録出来ません" do
       user = build(:user, first_name_kana: nil)
       user.valid?
-      expect(user.errors[:first_name_kana]).to include("can't be blank")
+      expect(user.errors[:first_name_kana]).to include("を入力してください", "は不正な値です")
     end
 
     #15. first_name_kanaはカタカナorひらがなで登録OK
@@ -104,31 +105,32 @@ describe User do
       expect(user).to be_valid
     end
 
-    #16. first_name_kanaはカタカナorひらがな以外では登録出来ないこと
-    it "first_name_kanaはカタカナorひらがな以外では登録出来ません" do
+    # 16. first_name_kanaがカタカナorひらがな以外では登録できないこと
+    it "first_name_kanaはカタカナorひらがな以外では登録出来ません " do
       user = build(:user, first_name_kana: "katakana")
-      expect(user.errors[:first_name_kana]).to include("はカタカナorひらがなで入力して下さい")
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("は不正な値です")
     end
 
     # 17. birth_yearが空だと登録出来ないこと
     it "birth_yearが空だと登録出来ません" do
       user = build(:user, birth_year: nil)
       user.valid?
-      expect(user.errors[:birth_year]).to include("can't be blank")
+      expect(user.errors[:birth_year]).to include("を入力してください")
     end
 
     # 18. birth_monthが空だと登録出来ないこと
     it "birth_monthが空だと登録出来ません" do
       user = build(:user, birth_month: nil)
       user.valid?
-      expect(user.errors[:birth_month]).to include("can't be blank")
+      expect(user.errors[:birth_month]).to include("を入力してください")
     end
 
     # 19. birth_dayが空だと登録出来ないこと
     it "birth_dayが空だと登録出来ません" do
       user = build(:user, birth_day: nil)
       user.valid?
-      expect(user.errors[:birth_day]).to include("can't be blank")
+      expect(user.errors[:birth_day]).to include("を入力してください")
     end
   end
 end
