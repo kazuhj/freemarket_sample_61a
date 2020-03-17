135a3794f68dbe57a2ee3073fb29632989f15a7d
kuroron985 <shigotoyou.taka@gmil.com>
2020-03-17 19:21:42 +0900

    [remove] 不要なコメントアウトを削除

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :image do
     image {File.open("#{Rails.root}/test/fixtures/files/images/夕映えの影.jpg")}
-    # image {File.open("#{Rails.root}/public/uploads/image/image/4/夕映えの影.jpg")}
     association :item
   end
 end
