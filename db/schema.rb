ca59c70caace957810069675b901b765cae323c4
kuroron985 <shigotoyou.taka@gmil.com>
2020-03-27 17:03:59 +0900

    [change] カラム追加に伴うスキーマーファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_050104) do
+ActiveRecord::Schema.define(version: 2020_03_25_061658) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture"
@@ -29,13 +29,11 @@ ActiveRecord::Schema.define(version: 2020_03_17_050104) do
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "customer_number", null: false
-    t.integer "year", null: false
-    t.integer "month", null: false
-    t.integer "security_code", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
